0f2f04d9e5894f2ba49ca774dc55388492db5ede
Brennon Loveless <brennon.loveless@gmail.com>
2023-01-02 10:46:29 -0800

    Only ignore root configuration.nix since it is a symlink on machines

---
+++ machines/foxmccloud/configuration.nix
@@ -0,0 +1,24 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/base/configuration.nix
+    ../../modules/users/brennon.nix
+  ];
+
+  networking.hostName = "foxmccloud"; # Define your hostname.
+  networking.interfaces.ens18.ipv4.addresses = [ {
+    address = "192.168.5.19";
+    prefixLength = 22;
+  } ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+}
+
