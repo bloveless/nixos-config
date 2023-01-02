c3c22d86775624d69968796aa65b62289a116d6e
Brennon Loveless <brennon.loveless@gmail.com>
2023-01-02 09:59:17 -0800

    Initial version for foxmccloud

---
+++ modules/users/brennon.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.brennon = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+  #     firefox
+  #     thunderbird
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXm5R5aXSC+CkrF4XhlqNqdfSzXpZKaM3arSfgNXjYE brennon@Brennon-Loveless-MacBook-Pro.local"
+    ];
+  };
+}
+
