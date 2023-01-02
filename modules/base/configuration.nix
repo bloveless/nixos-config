3eb7c6a7e19ba524a6b183c1f7de7771fabf9292
Brennon Loveless <brennon.loveless@gmail.com>
2023-01-02 11:04:24 -0800

    Added base configuration

---
+++ modules/base/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.defaultGateway = "192.168.4.1";
+  networking.nameservers = [ "192.168.5.201" ];
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    git
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "22.11";
+}
+
