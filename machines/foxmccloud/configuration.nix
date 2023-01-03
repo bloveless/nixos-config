14b3adf31e9cf3e0feca84a92104f43473e3af48
Brennon Loveless <brennon.loveless@gmail.com>
2023-01-02 21:06:11 -0800

    Remove systemd notify

--- machines/foxmccloud/configuration.nix
+++ machines/foxmccloud/configuration.nix
@@ -19,28 +19,18 @@
   } ];
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [
-  #   8080
-  # ];
+  networking.firewall.allowedTCPPorts = [
+    8080
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
 
-  # virtualisation.oci-containers.containers = {
-  #   nginx = {
-  #     image = "nginx:1.23.3";
-  #     autoStart = true;
-  #     ports = [ "0.0.0.0:8080:80" ];
-  #   };
-  # };
-
-  # systemd.services.podman-nginx.serviceConfig.User = "podmanager";
-
-    systemd.services.podman-nginx = {
+  systemd.services.podman-nginx = {
     enable = true;
-    wantedBy = [ "default.target" ]; 
+    wantedBy = [ "default.target" ];
     after = [ "network.target" ];
     description = "Nginx pod";
-    serviceConfig = 
-    let 
+    serviceConfig =
+    let
       podmancli = "${pkgs.bash}/bin/bash -l -c \"${config.virtualisation.podman.package}/bin/podman";
       endpodmancli = "\"";
       image = "nginx:1.23.3";
@@ -57,53 +47,14 @@
       ExecStart = "${podmancli} run " +
         "--rm " +
         "--name=${podname} " +
-        "--sdnotify=conmon " +
         "--log-driver=journald " +
         "-p '0.0.0.0:8080:80' " +
-        "${image} ${endpodmancli}"; 
+        "${image} ${endpodmancli}";
 
-      Type = "notify";
-      NotifyAccess = "all";
       ExecStop = "${podmancli} stop ${podname} ${endpodmancli}";
       ExecStopPost = cleanup_pod;
       Restart = "always";
       TimeoutStopSec = 15;
     };
   };
-
-  # systemd.services.podman-nginx = {
-  #   enable = true;
-  #   wantedBy = [ "default.target" ];
-  #   after = [ "network.target" ];
-  #   description = "Text nginx pod";
-  #   path = [
-  #     pkgs.shadow # for newuidmap and newgidmap binaries
-  #   ];
-  #   serviceConfig =
-  #   let
-  #     podmancli = "${config.virtualisation.podman.package}/bin/podman";
-  #     image_version = "1.23.3";
-  #     podname = "nginx";
-  #   in
-  #   {
-  #     User = "podmanager";
-  #     ExecStartPre= [
-  #       "${podmancli} stop -i ${podname}"
-  #       "${podmancli} rm -i ${podname}"
-  #     ];
-  #     ExecStart = "${podmancli} run " +
-  #       "--rm " +
-  #       "--name=${podname} " +
-  #       "--log-driver=journald " +
-  #       "-p '0.0.0.0:8080:80' " +
-  #       "docker.io/library/nginx:${image_version}";
-  #
-  #     ExecStop = "${podmancli} stop ${podname}";
-  #     ExecStopPost = "${podmancli} rm -i ${podname}";
-  #     Restart = "always";
-  #     TimeoutStopSec = 15;
-  #   };
-  # };
-
 }
-
