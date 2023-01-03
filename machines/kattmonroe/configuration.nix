b83c2b4b189cf36cb8f2e0d70f1136abf491bd2d
Brennon Loveless <brennon.loveless@gmail.com>
2023-01-02 21:11:34 -0800

    Final test by copying to katt

--- machines/kattmonroe/configuration.nix
+++ machines/kattmonroe/configuration.nix
@@ -18,7 +18,43 @@
   } ];
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [
+    8080
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+
+  systemd.services.podman-nginx = {
+    enable = true;
+    wantedBy = [ "default.target" ];
+    after = [ "network.target" ];
+    description = "Nginx pod";
+    serviceConfig =
+    let
+      podmancli = "${pkgs.bash}/bin/bash -l -c \"${config.virtualisation.podman.package}/bin/podman";
+      endpodmancli = "\"";
+      image = "nginx:1.23.3";
+      podname = "nginx";
+      cleanup_pod = [
+        "${podmancli} stop -i ${podname} ${endpodmancli}"
+        "${podmancli} rm -i ${podname} ${endpodmancli}"
+      ];
+    in
+    {
+      User = "podmanager";
+      WorkingDirectory = "/home/podmanager";
+      ExecStartPre = cleanup_pod;
+      ExecStart = "${podmancli} run " +
+        "--rm " +
+        "--name=${podname} " +
+        "--log-driver=journald " +
+        "-p '0.0.0.0:8080:80' " +
+        "${image} ${endpodmancli}";
+
+      ExecStop = "${podmancli} stop ${podname} ${endpodmancli}";
+      ExecStopPost = cleanup_pod;
+      Restart = "always";
+      TimeoutStopSec = 15;
+    };
+  };
 }
 
