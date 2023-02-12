d6e8a47fe99dcbae5407bd10de4a30039558ef97
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-12 14:54:01 -0800

    Added consul

--- modules/nomad/base.nix
+++ modules/nomad/base.nix
@@ -11,9 +11,29 @@
     4646 # nomad http api
     4647 # nomad internal rpc
     4648 # nomad gossip protocol
+    8600 # consul dns
+    8500 # consul http api
+    8501 # consul https api
+    8502 # consul grpc api
+    8503 # consul grpc api tls
+    8301 # consul serf lan
+    8302 # consul serf wan
+    8300 # consul rpc address
+  ];
+  networking.firewall.allowedTCPPortRanges = [
+    { from = 21000; to = 21255; } # consul proxy sidecar
   ];
   networking.firewall.allowedUDPPorts = [
     4648 # nomad gossip protocol
+    8600 # consul dns
+    8501 # consul https api
+    8502 # consul grpc api
+    8503 # consul grpc api tls
+    8301 # consul serf lan
+    8302 # consul serf wan
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    { from = 21000; to = 21255; } # consul proxy sidecar
   ];
 
   users.groups.nomad = {};
--- modules/nomad/server.nix
+++ modules/nomad/server.nix
@@ -1,6 +1,73 @@
 { config, pkgs, ... }:
 
 {
+  import "./secrets.nix" consul;
+
+  environment.etc = {
+    "consul.d/certs/consul-agent-ca-key.pem".text = consul."consul-agent-ca-key.pem";
+
+    "consul.d/certs/consul-agent-ca.pem".text = consul."consul-agent-ca.pem";
+
+    "consul.d/certs/homelab01-server-consul-0-key.pem".text = consul."homelab01-server-consul-0-key.pem";
+
+    "consul.d/certs/homelab01-server-consul-0.pem".text = consul."homelab01-server-consul-0.pem";
+
+    "consul.d/consul.hcl".text = ''
+      datacenter = "dc1"
+      encrypt = "${consul.encryption_key}"
+      verify_incoming = true
+      verify_outgoing = true
+      verify_server_hostname = true
+
+      ca_file = "/etc/consul.d/certs/consul-agent-ca.pem"
+      cert_file = "/etc/consul.d/certs/homelab01-server-consul-0.pem"
+      key_file = "/etc/consul.d/certs/homelab01-server-consul-0-key.pem"
+
+      auto_encrypt {
+        allow_tls = true
+      }
+
+      retry_join = [ "192.168.5.20", "192.168.5.57", "192.168.5.108" ]
+
+      acl {
+        enabled = true
+        default_policy = "allow"
+        enable_token_persistence = true
+      }
+
+      performance {
+        raft_multiplier = 1
+      }
+
+      ui_config {
+        enabled = true
+      }
+    '';
+    
+    "consul.d/server.hcl".text = ''
+      server = true
+      bootstrap_expect = 3
+      client_addr = "0.0.0.0"
+
+      connect {
+        enabled = true
+      }
+
+      addresses {
+        grpc = "127.0.0.1"
+      }
+
+      ports {
+        grpc  = 8502
+      }
+    '';
+
+  };
+
+  services.consul = {
+    enable = true;
+  };
+
   services.nomad = {
     enable = true;
     package = pkgs.nomad_1_4;
