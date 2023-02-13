f3b8df98762e4e92dcac81078192f6afc7d19537
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-12 18:15:40 -0800

    Removed nomad join settings since it will work automatically with consul now

--- modules/nomad/server.nix
+++ modules/nomad/server.nix
@@ -81,12 +81,6 @@ with lib;
         server = {
           enabled = true;
           bootstrap_expect = 3;
-
-          server_join = {
-            retry_join = [ "192.168.5.20" "192.168.5.57" "192.168.5.108" ];
-            retry_max = 3;
-            retry_interval = "15s";
-          };
         };
       };
     };
