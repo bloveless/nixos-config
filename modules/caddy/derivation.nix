1795161b44e04f53e57c6392f071e52693ca7340
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-17 22:28:19 +0000

    Use caddy commit version instead

--- modules/caddy/derivation.nix
+++ modules/caddy/derivation.nix
@@ -2,7 +2,7 @@
 
 let
   # Set the version of Caddy to build
-  version = "2.6.4";
+  caddy_commit = "0db29e2ce9799f652f3d16fd5aed6e426d23bd0a"; # 2.6.4 tag
 
   # Set the version of the Cloudflare module to include
   cloudflare_version = "latest";
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
 
   # Build Caddy with the Cloudflare module
   buildPhase = ''
-    ${pkgs.xcaddy}/bin/xcaddy build --with github.com/caddyserver/caddy/v2=github.com/caddyserver/caddy@${version} --with github.com/caddy-dns/cloudflare@${cloudflare_version}
+    ${pkgs.xcaddy}/bin/xcaddy build --with github.com/caddyserver/caddy/v2=github.com/caddyserver/caddy@${caddy_commit} --with github.com/caddy-dns/cloudflare@${cloudflare_version}
   '';
 
   # Install Caddy
