64b22ccd647abe7fb71734cf34b23ace6329d9b6
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-17 22:19:43 +0000

    Use xcaddy only

--- modules/caddy/derivation.nix
+++ modules/caddy/derivation.nix
@@ -11,24 +11,11 @@ in
 stdenv.mkDerivation rec {
   name = "caddy-${version}-with-cloudflare-${cloudflare_version}";
 
-  src = fetchFromGitHub {
-    owner = "caddyserver";
-    repo = "caddy";
-    rev = "v${version}";
-    sha256 = "3a3+nFHmGONvL/TyQRqgJtrSDIn0zdGy9YwhZP17mU0=";
-  };
-
   buildInputs = [ go ];
 
   # Build Caddy with the Cloudflare module
   buildPhase = ''
-    export GOPATH=$PWD/.gopath
-    export GOBIN=$PWD/bin
-
-    mkdir -p $GOPATH/src/github.com/caddyserver
-    ln -s $src $GOPATH/src/github.com/caddyserver/caddy
-
-    cd $GOPATH/src/github.com/caddyserver/caddy/cmd/caddy
+    export CADDY_VERSION=${version}
     ${pkgs.xcaddy}/bin/xcaddy build --with github.com/caddy-dns/cloudflare@${cloudflare_version}
   '';
 
