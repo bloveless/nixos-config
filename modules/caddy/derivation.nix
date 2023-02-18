6170f5fadcb8461a7647211c23e2be61d7b47ad2
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-18 04:48:16 +0000

    Another attempt

--- modules/caddy/derivation.nix
+++ modules/caddy/derivation.nix
@@ -1,75 +1,63 @@
-{ lib, buildGo119Module, fetchFromGitHub, nixosTests, plugins ? [ ], pkgs }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, nixosTests
+, caddy
+, testers
+, installShellFiles
+}:
 let
   version = "2.6.4";
   dist = fetchFromGitHub {
     owner = "caddyserver";
     repo = "dist";
     rev = "v${version}";
-    sha256 = "sha256:1nlphjg5wh5drpwkm4cczrkxdzbv72ll7hp5x7z6ww8pzz3q10b3";
+    hash = "sha256-SJO1q4g9uyyky9ZYSiqXJgNIvyxT5RjrpYd20YDx8ec=";
   };
-  imports = lib.flip lib.concatMapStrings plugins (pkg: "	_ \"${pkg}\"\n");
-  main = ''
-    package main
-
-    import (
-    	caddycmd "github.com/caddyserver/caddy/v2/cmd"
-    	_ "github.com/caddyserver/caddy/v2/modules/standard"
-    ${imports}
-    )
-
-    func main() {
-    	caddycmd.Main()
-    }
-  '';
-in buildGo119Module {
+in
+buildGoModule {
   pname = "caddy";
   inherit version;
-  runVend = true;
-
-  subPackages = [ "cmd/caddy" ];
 
   src = fetchFromGitHub {
     owner = "caddyserver";
     repo = "caddy";
     rev = "v${version}";
-    sha256 = "sha256:1nlphjg5wh5drpwkm4cczrkxdzbv72ll7hp5x7z6ww8pzz3q10b3";
+    hash = "sha256-3a3+nFHmGONvL/TyQRqgJtrSDIn0zdGy9YwhZP17mU0=";
   };
 
-  vendorSha256 = "sha256-s3vfTOdjIEs9ds9EtDbBpmTwPbkDkNohacUtrReZ050=";
+  vendorHash = "sha256-toi6efYZobjDV3YPT9seE/WZAzNaxgb1ioVG4txcuXM=";
 
-  nativeBuildInputs = [ pkgs.breakpointHook ];
+  subPackages = [ "cmd/caddy" ];
 
-  overrideModAttrs = (_: {
-    preBuild = ''
-      go get github.com/caddy-dns/cloudflare
-      echo '${main}' > cmd/caddy/main.go
-    '';
-    postInstall = "cp go.sum go.mod $out/ && ls $out/";
-  });
+  ldflags = [
+    "-s" "-w"
+    "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
+  ];
 
-  postPatch = ''
-    echo '${main}' > cmd/caddy/main.go
-    cat cmd/caddy/main.go
-  '';
-
-  postConfigure = ''
-    cp vendor/go.sum ./
-    cp vendor/go.mod ./
-  '';
+  nativeBuildInputs = [ installShellFiles ];
 
   postInstall = ''
     install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
-
     substituteInPlace $out/lib/systemd/system/caddy.service --replace "/usr/bin/caddy" "$out/bin/caddy"
     substituteInPlace $out/lib/systemd/system/caddy-api.service --replace "/usr/bin/caddy" "$out/bin/caddy"
+    installShellCompletion --cmd metal \
+      --bash <($out/bin/caddy completion bash) \
+      --zsh <($out/bin/caddy completion zsh)
   '';
 
-  passthru.tests = { inherit (nixosTests) caddy; };
+  passthru.tests = {
+    inherit (nixosTests) caddy;
+    version = testers.testVersion {
+      command = "${caddy}/bin/caddy version";
+      package = caddy;
+    };
+  };
 
   meta = with lib; {
     homepage = "https://caddyserver.com";
-    description = "Fast, cross-platform HTTP/2 web server with automatic HTTPS";
+    description = "Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS";
     license = licenses.asl20;
-    maintainers = with maintainers; [ Br1ght0ne techknowlogick ];
+    maintainers = with maintainers; [ Br1ght0ne indeednotjames techknowlogick ];
   };
 }
