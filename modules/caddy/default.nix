0767695bd0c01793e58d443156f46b409bbc1916
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-18 04:57:56 +0000

    Another attempt

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -20,7 +20,7 @@ let
     import (
     	caddycmd "github.com/caddyserver/caddy/v2/cmd"
     	_ "github.com/caddyserver/caddy/v2/modules/standard"
-        _ "github.com/caddy-dns/cloudflare"
+    	_ "github.com/caddy-dns/cloudflare"
     )
 
     func main() {
