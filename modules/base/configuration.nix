3317e74978b5eb219e44f85eb7f41500c10a7364
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-23 06:07:02 +0000

    Deploying

--- modules/base/configuration.nix
+++ modules/base/configuration.nix
@@ -16,21 +16,12 @@
     wget
     curl
     git
-    libva-utils
   ];
-
-  # Enable GPU
-  # nixpkgs.config.packageOverrides = pkgs: {
-  #   vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  # };
 
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      # vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      # vaapiVdpau
-      # libvdpau-va-gl
+      intel-media-driver
     ];
   };
 
