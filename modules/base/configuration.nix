c2c64f310bd45cee24e6c94bb60fc0aae7d13773
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-23 05:59:54 +0000

    Deploying

--- modules/base/configuration.nix
+++ modules/base/configuration.nix
@@ -19,6 +19,22 @@
     libva-utils
   ];
 
+  # Enable GPU
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
