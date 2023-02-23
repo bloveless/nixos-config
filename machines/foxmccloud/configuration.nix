9e249f66ec1847b8ed769e01476b00d0e055378a
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-23 05:58:08 +0000

    Deploying

--- machines/foxmccloud/configuration.nix
+++ machines/foxmccloud/configuration.nix
@@ -14,6 +14,19 @@
     ../../modules/nomad/client.nix
   ];
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
   environment.etc = {
     "nomad.d/volumes.hcl".text = ''
       client {
