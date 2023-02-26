e3bc0695b89c6e4c7af44703488faa3d972f74af
Brennon Loveless <brennon.loveless@gmail.com>
2023-02-26 08:41:51 -0800

    Deploying

--- modules/nomad/client.nix
+++ modules/nomad/client.nix
@@ -65,7 +65,7 @@ with lib;
         plugin "docker" {
           config {
             allow_privileged = true
-            allow_caps = [ ["audit_write", "chown", "dac_override", "fowner", "fsetid", "kill", "mknod", "net_bind_service", "setfcap", "setgid", "setpcap", "setuid", "sys_chroot", "net_admin" ]
+            allow_caps = ["audit_write", "chown", "dac_override", "fowner", "fsetid", "kill", "mknod", "net_bind_service", "setfcap", "setgid", "setpcap", "setuid", "sys_chroot", "net_admin" ]
             
             volumes {
                 enabled = true
