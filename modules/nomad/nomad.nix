f182d4be1b68ea76748013fd930f674e4622975a
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-04 13:56:21 -0800

    Deploying

--- modules/nomad/nomad.nix
+++ modules/nomad/nomad.nix
@@ -13,7 +13,7 @@ in buildGo120Module (rec {
   inherit pname version sha256 vendorSha256;
 
   subPackages = [ "." ];
-  passthru.tests.nomad = nixosTests.nomad;
+  passthru.tests = null;
 
   src = fetchFromGitHub {
     owner = "hashicorp";
