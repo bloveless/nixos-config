f6c8b587e5d78778eebb1939a03ce92a68f44849
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-04 11:13:26 -0800

    Deploying

--- modules/nomad/nomad.nix
+++ modules/nomad/nomad.nix
@@ -7,7 +7,7 @@ let
   pname = "nomad";
   version = "1.5.0";
   sha256 = "sha256-xaMt57g2TV+LG5NRmIvKqg/Ljf2BFcHayuxC+Fz/9Ys=";
-  vendorSha256 = "sha256-ttP7pzsIBd2S79AUcbOeVG71Mb5qK706rq5DkT41VqM=";
+  vendorSha256 = "sha256-qU1gpN9T3b+onWgs2lJ01Wc0zzWwyndbvoja6fZsXFE=";
 
 in buildGo120Module (rec {
   inherit pname version sha256 vendorSha256;
