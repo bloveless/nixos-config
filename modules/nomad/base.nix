b83344be227ee403cfbd97dafac73f589a0cee9c
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-05 08:52:49 -0800

    Deploying

--- modules/nomad/base.nix
+++ modules/nomad/base.nix
@@ -15,8 +15,8 @@ in {
     enable = true;
     package = nomad-1-5;
     dropPrivileges = false;
-    extraSettingsPlugins = [ cni-plugins-1-2 ];
-    extraPackages = [ cni-plugins-1-2 ];
+    extraSettingsPlugins = [ pkgs.cni-plugins ];
+    extraPackages = [ pkgs.cni-plugins ];
     extraSettingsPaths = [
       "/etc/nomad.d"
     ];
