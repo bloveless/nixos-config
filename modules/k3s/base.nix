b16f14c7f1fb344ca6d56ccfb875cfe084faaf8f
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-06 12:28:23 -0800

    Deploying

--- modules/k3s/base.nix
+++ modules/k3s/base.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  k3s_1_26= pkgs.callPackage ./k3s/1_26/default.nix {};
+  k3s_1_26= pkgs.callPackage ./1_26/default.nix {};
 in {
   environment.systemPackages = [
     k3s_1_26
