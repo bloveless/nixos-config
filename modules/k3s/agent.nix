2e479dd66ef2215befc9a6ff08e247499b494946
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-18 12:41:58 -0700

    Deploying

--- modules/k3s/agent.nix
+++ modules/k3s/agent.nix
@@ -9,7 +9,6 @@
 
   networking.firewall.allowedTCPPorts = [
     10250 # k3s kubelet metrics
-    443 # consul api gateway
   ];
   networking.firewall.allowedTCPPortRanges = [];
   networking.firewall.allowedUDPPorts = [
