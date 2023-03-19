ae1ebb760a504e9b4b73557419e93495d22feb04
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-18 21:27:44 -0700

    Deploying

--- machines/andross02/hardware-configuration.nix
+++ machines/andross02/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "ip_vs" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
