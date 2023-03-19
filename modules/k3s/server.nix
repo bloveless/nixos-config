50708ff9322f8760e7820a5f8741d5686269ba45
Brennon Loveless <brennon.loveless@gmail.com>
2023-03-19 10:56:33 -0700

    Deploying

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -24,12 +24,14 @@
 
     networking.firewall.allowedTCPPorts = [
       6443 # k3s api
+      7934 # purelb memberlist port
       10250 # k3s kubelet metrics
     ];
     networking.firewall.allowedTCPPortRanges = [
       { from = 2379; to = 2380; } # k3s embedded etcd
     ];
     networking.firewall.allowedUDPPorts = [
+      7934 # purelb memberlist port
       8472 # k3s flannel vxlan
       51820 # k3s flannel wireguard
       51821 # k3s flannel wireguard ipv6
